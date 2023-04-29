dc248d10178d0f1ad6ec6e64623530e4f72b9b5e
pravalgautam <pravalgautam79@gmail.com>
2023-04-30 01:44:18 +0530

    Initial Commit

---
+++ Flag Bee/Flag_BeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flag_BeeApp.swift
+//  Flag Bee
+//
+//  Created by Praval Gautam on 30/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Flag_BeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
